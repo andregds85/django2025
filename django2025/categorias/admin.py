98d11548de422ef9a6d8eb185261a8220ff16e12
andregds85@gmail.com <andregds85@gmail.com>
2021-01-31 00:14:25 -0300

    teste
    teste

--- django2025/categorias/admin.py
+++ django2025/categorias/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from . models import *
 
-admin.site.register(categoria)
-
+admin.site.register(categorias)
--- django2025/categorias/models.py
+++ django2025/categorias/models.py
@@ -3,11 +3,5 @@ from django.db import models
 class categoria(models.Model):
         nome = models.CharField(max_length=100)
         cnes = models.CharField(max_length=100)
-     
- 
-
-
-
-
   
-
+ 
